7690e8650dc3fc340edd505310e1e0a55ba9d794
Vladan <vladan.marchenko@gmail.com>
2022-01-27 09:53:10 +0200

    first commit

---
+++ src/main/java/com/example/vladanbookstore/VladanbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.vladanbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VladanbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VladanbookstoreApplication.class, args);
+	}
+
+}
