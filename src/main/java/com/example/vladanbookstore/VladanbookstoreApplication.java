56b7d208c87bf13f5c1f42942e95ab5ece2d9e3e
Vladan <vladan.marchenko@gmail.com>
2022-02-09 17:56:04 +0200

    second commit

--- src/main/java/com/example/vladanbookstore/VladanbookstoreApplication.java
+++ src/main/java/com/example/vladanbookstore/VladanbookstoreApplication.java
@@ -1,13 +1,33 @@
 package com.example.vladanbookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.vladanbookstore.model.Book;
+import com.example.vladanbookstore.model.BookRepository;
 
 @SpringBootApplication
 public class VladanbookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VladanbookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			System.out.println("Save a couple of books");
+			repository.save(new Book("The Man Who Laughs", "Victor Hugo", 1869, "978-14954-41936", 14.90));
+			repository.save(new Book("Ulysses", "James Joyce", 1922, "978-95249-52262", 30.00));
+
+			System.out.println("Fetch all books");
+			for (Book book : repository.findAll()) {
+				System.out.println(book.toString());
+			}
+		};
+
 	}
 
 }
--- src/main/java/com/example/vladanbookstore/web/BookController.java
+++ src/main/java/com/example/vladanbookstore/web/BookController.java
@@ -1,15 +1,53 @@
 package com.example.vladanbookstore.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.vladanbookstore.model.Book;
+import com.example.vladanbookstore.model.BookRepository;
+
 @Controller
-@ResponseBody
 public class BookController {
-	@RequestMapping("/index")
-	public String listboks() {
-		return "placeholder";
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping(value = {"/", "/booklist"})
+	public String listbooks(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
 
+
+
+
+
+	
+	
+	
+	
