7ebd381bc8dfb1aa3e5cb3e88506175f76b12b68
Vladan <vladan.marchenko@gmail.com>
2022-02-09 19:51:15 +0200

    third commit

--- src/main/java/com/example/vladanbookstore/web/BookController.java
+++ src/main/java/com/example/vladanbookstore/web/BookController.java
@@ -1,10 +1,13 @@
 package com.example.vladanbookstore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,9 +40,23 @@ public class BookController {
 	}
 	
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-		repository.deleteById(bookId);
+	public String deleteBook(@PathVariable("id") Long id, Model model) {
+		repository.deleteById(id);
 		return "redirect:../booklist";
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("book", repository.findById(id));
+		return "editbook";
+	}
+	
+	@RequestMapping(value = "/process", method = RequestMethod.POST)
+	public String saveBook(@ModelAttribute Book book) {
+		repository.save(book);
+		
+		
+		return "redirect:/booklist";
 	}
 }
 
@@ -48,6 +65,5 @@ public class BookController {
 
 
 	
-	
-	
+
 	
