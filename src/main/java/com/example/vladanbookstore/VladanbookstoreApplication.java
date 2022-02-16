aef47b31b8b3e4d05bbe9fea8c27dd3a8e9f103d
Vladan <vladan.marchenko@gmail.com>
2022-02-16 14:16:04 +0200

    new commit

--- src/main/java/com/example/vladanbookstore/VladanbookstoreApplication.java
+++ src/main/java/com/example/vladanbookstore/VladanbookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.vladanbookstore.model.Book;
 import com.example.vladanbookstore.model.BookRepository;
+import com.example.vladanbookstore.model.Category;
+import com.example.vladanbookstore.model.CategoryRepository;
 
 @SpringBootApplication
 public class VladanbookstoreApplication {
@@ -16,11 +18,21 @@ public class VladanbookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
 		return (args) -> {
+			
+			System.out.println("Save a couple of categories");
+			crepository.save(new Category("Novel"));
+			crepository.save(new Category("Science Fiction"));
+			crepository.save(new Category("Detective"));
+			crepository.save(new Category("Thriller"));
+			crepository.save(new Category("Adventure"));
+			
+		
 			System.out.println("Save a couple of books");
-			repository.save(new Book("The Man Who Laughs", "Victor Hugo", 1869, "978-14954-41936", 14.90));
-			repository.save(new Book("Ulysses", "James Joyce", 1922, "978-95249-52262", 30.00));
+			repository.save(new Book("The Man Who Laughs", "Victor Hugo", 1869, "978-14954-41936", 17.00, crepository.findByName("Novel").get(0)));
+			repository.save(new Book("Ulysses", "James Joyce", 1922, "978-95249-52262", 25.00, crepository.findByName("Novel").get(0)));
+			repository.save(new Book("Hard To Be A God", "Arkady & Boris Strugatsky", 1964, "978-14523-52612", 15.00, crepository.findByName("Science Fiction").get(0)));
 
 			System.out.println("Fetch all books");
 			for (Book book : repository.findAll()) {
--- src/main/java/com/example/vladanbookstore/web/BookController.java
+++ src/main/java/com/example/vladanbookstore/web/BookController.java
@@ -1,25 +1,24 @@
 package com.example.vladanbookstore.web;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.example.vladanbookstore.model.Book;
 import com.example.vladanbookstore.model.BookRepository;
+import com.example.vladanbookstore.model.CategoryRepository;
 
 @Controller
 public class BookController {
 	@Autowired
 	private BookRepository repository;
+	
+	@Autowired
+	private CategoryRepository crepository;
 	
 	@RequestMapping(value = {"/", "/booklist"})
 	public String listbooks(Model model) {
@@ -30,6 +29,7 @@ public class BookController {
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
+		model.addAttribute("categories", crepository.findAll());
 		return "addbook";
 	}
 	
@@ -47,23 +47,13 @@ public class BookController {
 	
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public String showUpdateForm(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("book", repository.findById(id));
+		Optional <Book> book = repository.findById(id);
+		model.addAttribute("book", book);
+		model.addAttribute("categories", crepository.findAll());
 		return "editbook";
 	}
 	
-	@RequestMapping(value = "/process", method = RequestMethod.POST)
-	public String saveBook(@ModelAttribute Book book) {
-		repository.save(book);
+
 		
-		
-		return "redirect:/booklist";
-	}
+	
 }
-
-
-
-
-
-	
-
-	
