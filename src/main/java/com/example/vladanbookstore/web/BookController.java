ebb504f028cce995e33c97c0583748a515f6dd40
Vladan <vladan.marchenko@gmail.com>
2022-03-02 20:24:23 +0200

    another commit

--- src/main/java/com/example/vladanbookstore/web/BookController.java
+++ src/main/java/com/example/vladanbookstore/web/BookController.java
@@ -1,5 +1,6 @@
 package com.example.vladanbookstore.web;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.vladanbookstore.model.Book;
 import com.example.vladanbookstore.model.BookRepository;
 import com.example.vladanbookstore.model.CategoryRepository;
@@ -25,6 +29,17 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	@RequestMapping(value="/books/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
+	}
+	
 	
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
