579636711657aa3ff1aa4b0f87f343a8378d886c
Vladan <vladan.marchenko@gmail.com>
2022-03-09 20:46:20 +0200

    2securingwebapp

--- src/main/java/com/example/vladanbookstore/VladanbookstoreApplication.java
+++ src/main/java/com/example/vladanbookstore/VladanbookstoreApplication.java
@@ -9,6 +9,9 @@ import com.example.vladanbookstore.model.Book;
 import com.example.vladanbookstore.model.BookRepository;
 import com.example.vladanbookstore.model.Category;
 import com.example.vladanbookstore.model.CategoryRepository;
+import com.example.vladanbookstore.model.User;
+import com.example.vladanbookstore.model.UserRepository;
+
 
 @SpringBootApplication
 public class VladanbookstoreApplication {
@@ -18,7 +21,7 @@ public class VladanbookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			
 			System.out.println("Save a couple of categories");
@@ -33,7 +36,14 @@ public class VladanbookstoreApplication {
 			repository.save(new Book("The Man Who Laughs", "Victor Hugo", 1869, "978-14954-41936", 17.00, crepository.findByName("Novel").get(0)));
 			repository.save(new Book("Ulysses", "James Joyce", 1922, "978-95249-52262", 25.00, crepository.findByName("Novel").get(0)));
 			repository.save(new Book("Hard To Be A God", "Arkady & Boris Strugatsky", 1964, "978-14523-52612", 15.00, crepository.findByName("Science Fiction").get(0)));
-
+			
+			System.out.println("Save users");
+			User user1 = new User("polo", "$2a$12$q1mTAjpE5P8ZViJicFUSjeYkaonrD.z4pXTKzAs8isVArsGKdaWCC", "polo@mail.com", "USER");
+			User user2 = new User("kosto", "$2a$12$nG6yvvx4SHk7VBH9tLcaDeAu0Qz/Z5jwDzuXIle3a.YR45.p1ILAW", "kosto@mail.com", "USER");
+			urepository.save(user1);
+			urepository.save(user2);
+			
+			
 			System.out.println("Fetch all books");
 			for (Book book : repository.findAll()) {
 				System.out.println(book.toString());
--- src/main/java/com/example/vladanbookstore/WebSecurityConfig.java
+++ src/main/java/com/example/vladanbookstore/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.vladanbookstore;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,22 +14,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.example.vladanbookstore.web.UserDetailServiceImpl;
+
 
 @Configuration
-@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailServiceImpl userDetailsService;	
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests().antMatchers("/css/**").permitAll()
+        .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
         .and()
-        .authorizeRequests().anyRequest().authenticated()
-        .and()
+        .authorizeRequests()
+          .anyRequest().authenticated()
+          .and()
       .formLogin()
           .defaultSuccessUrl("/booklist", true)
           .permitAll()
@@ -35,32 +44,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .logout()
           .permitAll();
     }
-
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        List<UserDetails> users = new ArrayList();
-
-        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-        UserDetails user = User
-        		.withUsername("user")
-        		.password(passwordEncoder.encode("user"))
-        		.roles("USER")
-        		.build();
-
-        users.add(user);
-
-        user = User
-        		.withUsername("admin")
-        		.password(passwordEncoder.encode("admin"))
-        		.roles("USER", "ADMIN")
-        		.build();
-
-    	users.add(user);
-
-        return new InMemoryUserDetailsManager(users);
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
-
 }
